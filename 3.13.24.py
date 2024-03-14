fc61407012e9206b15751be0f2c03ab85b1e9f84
Ayushkhanal9 <aayush668@gmail.com>
2024-03-14 11:54:34 -0400

    Search Insert positions

--- 3.13.24.py
+++ 3.13.24.py
@@ -47,3 +47,16 @@ def guessNumber(n: int) -> int:
         else:
             return mid
     return -1
+#search insert positions. We use binary search method to do this
+def searchInsert(nums: List[int], target: int) -> int:
+    low = 0
+    high= len(nums)-1
+    while low <= high:
+        mid = low+(high-low)//2
+        if target<nums[mid]:
+            high = mid-1
+        elif target > nums[mid] :
+            low= mid+1
+        else:
+            return mid
+    return low
