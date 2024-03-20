1ca96cfd73634ddd0901ea2bad40b7d2f7d85030
Ayushkhanal9 <aayush668@gmail.com>
2024-03-20 18:29:26 -0400

    number palindrome

--- Reviews.py
+++ Reviews.py
@@ -7,3 +7,15 @@ class Solution:
                 return[mappy[snum],i]
             mappy[nums[i]]=i
         return []
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num = str(x)
+        left = 0
+        right = len(num)-1
+        is_true= True
+        while left<right:
+            if num[left]!=num[right]:
+                return False
+            left+=1
+            right-=1
+        return True
