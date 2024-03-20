d81b57bef8b64abfefa53642edcd1d74ff020f3a
Ayushkhanal9 <aayush668@gmail.com>
2024-03-20 19:11:36 -0400

    Roman to numerical

--- Reviews.py
+++ Reviews.py
@@ -19,3 +19,17 @@ class Solution:
             left+=1
             right-=1
         return True
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        mappy={ 'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        tot=0
+        for i in range(len(s)):
+            if s[i-1]=='I' and s[i] in ['V','X'] and i !=0:
+                tot=tot-2+mappy[s[i]]
+            elif s[i-1]=='X' and s[i] in ['L','C']and i !=0:
+                tot=tot-20+mappy[s[i]]   
+            elif s[i-1]=='C' and s[i] in ['D','M']and i !=0:
+                tot=tot-200+mappy[s[i]]       
+            else:
+                tot+=mappy[s[i]]
+        return tot
