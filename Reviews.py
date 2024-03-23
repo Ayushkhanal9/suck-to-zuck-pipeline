034b70f71af8a2275ef8229cecd3d5433387b4f5
Ayushkhanal9 <aayush668@gmail.com>
2024-03-22 22:35:02 -0400

    another day another dollar

--- Reviews.py
+++ Reviews.py
@@ -84,4 +84,17 @@ class Solution:
                 else:
                     stack.pop()
         return not stack        
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid < x:
+                left = mid + 1
+            elif mid * mid > x:
+                right = mid -1
+            else:
+                return mid
+        return right
 
