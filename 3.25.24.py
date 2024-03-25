9c863f99aa4434cbf72f3b9a394e8bb1c4b66d9a
Ayushkhanal9 <aayush668@gmail.com>
2024-03-25 19:26:41 -0400

    add biggest to the right

--- 3.25.24.py
+++ 3.25.24.py
@@ -15,3 +15,23 @@ class Solution:
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         return nums*2
+
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        ans=[]
+        ans.append(-1)
+        biggest=0
+        if len(arr)==1:
+            return [-1]
+        for i in range(len(arr)-1,-1,-1):
+            if i==0:
+                return reversed(ans)
+            biggest=max(biggest,arr[i])
+            if i==len(arr)-1:
+                ans.append(biggest)
+            elif biggest>arr[i-1]:
+                ans.append(biggest)
+            else:
+                ans.append(biggest)
+        return -1
