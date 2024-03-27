1d6965baf71d774063930a093ae0405df963de4d
Ayushkhanal9 <aayush668@gmail.com>
2024-03-27 13:44:52 -0400

    linked list

--- 3.25.24.py
+++ 3.25.24.py
@@ -61,3 +61,34 @@ class Solution:
             else:
                 counter+=1
         return counter if counter>0 else value
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None and list2 is None:
+            return None
+        elif list1 is None and list2 is not None:
+            return list2
+        elif list1 is not None and list2 is None:
+            return list1
+        if list1.val <= list2.val:
+            head = list1
+            list1 = list1.next
+        else:
+            head = list2
+            list2 = list2.next
+        curr = head
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        if list1:
+            curr.next = list1
+        elif list2:
+            curr.next = list2
+
+        return head
