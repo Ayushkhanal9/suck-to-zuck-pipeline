41379dd88df137ae2374ce8265eece195d4d5ba1
Ayushkhanal9 <aayush668@gmail.com>
2024-04-28 12:24:45 -0400

    stairs problem

--- pytocs.cs
+++ pytocs.cs
@@ -69,3 +69,23 @@ public class Solution {
         return counter > 0 ? counter : value;
     }
 }
+
+public class Solution {
+    public int ClimbStairs(int n) {
+        if (n == 1) {
+            return 1; 
+        }
+        if (n == 2) {
+            return 2; 
+        }
+        int first = 1;
+        int second = 2;
+        int result = 0;
+        for (int i = 3; i <= n; i++) {
+            result = first + second;
+            first = second; 
+            second = result; 
+        }
+        return result;
+    }
+}
