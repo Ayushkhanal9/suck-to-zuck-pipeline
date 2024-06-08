b8b8778adadd98259b41f039e44762e6513eae37
Ayushkhanal9 <aayush668@gmail.com>
2024-06-07 22:48:28 -0400

    only positive cumulative sum

--- leetcodeprobs/cumulativeSum.py
+++ leetcodeprobs/cumulativeSum.py
@@ -4,3 +4,14 @@ def getCumulativeSum(self, arr: List[int]) -> List[int]:
         if i !=0:
             arr[i]=arr[i]+arr[i-1]
     return arr
+def getPositiveCumulativeSum(self, arr: List[int]) -> List[int]:
+    n=len(arr)
+    posCum=[]
+    if arr[0]>0:
+        posCum.append(arr[0])
+    for i in range(1,n):
+        if i !=0:
+            arr[i]=arr[i-1]+arr[i]
+            if arr[i]>0:
+                posCum.append(arr[i])
+    return posCum
